055b46ef2f9eb395a226746381585bd3e5b9087a
nikola-velemir <vnexa021@gmail.com>
2023-12-03 18:50:41 +0100

    [add] delete element

---
+++ btree/btree/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"awesomeProject/btree"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+func main() {
+	t := btree.NewBTree(4) // A B-Tree with minimum degree 3
+
+	var testS string = ""
+	randa := rand.Intn(10000-1) + 1
+	for i := 1; i <= 10000; i++ {
+		if i == randa {
+			testS = RandStringRunes(10)
+			t.Insert(testS)
+		} else {
+			t.Insert(RandStringRunes(10))
+
+		}
+	}
+
+	output := t.Search(testS)
+	t.Delete(testS)
+	output = t.Search(testS)
+	fmt.Println(output)
+
+}
