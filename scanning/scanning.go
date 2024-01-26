b00b17a42fa13187cdd482155a441393138a0451
Nikson <nikolaking88@gmail.com>
2024-01-26 20:50:00 +0100

    Dodati neki komentari koji povecavaju preglednost koda i izmenjeno testiranje obe vrste skeniranja.

--- scanning/scanning.go
+++ scanning/scanning.go
@@ -41,9 +41,11 @@ func PREFIX_SCAN(prefix string, pageNumber, pageSize int) []DataType {
 func RANGE_SCAN(keyRange [2]int, pageNumber, pageSize int) []DataType {
 	var result []DataType
 
+	// Implement pagination
 	startIndex := (pageNumber - 1) * pageSize
 	endIndex := startIndex + pageSize
 
+	// iterate through resources and find elements
 	for key := range table {
 		if strings.Compare(key, keyRange) >= 0 {
 			result = append(result, DataType{Key: key, Value: value})
@@ -58,6 +60,8 @@ func RANGE_SCAN(keyRange [2]int, pageNumber, pageSize int) []DataType {
 	if endIndex > len(result) {
 		endIndex = len(result)
 	}
+
+	// Return the paginated result
 	return result[startIndex:endIndex]
 }
 
@@ -67,11 +71,20 @@ func main() {
 	pageNumber := 1
 	pageSize := 2
 
-	result := PREFIX_SCAN(prefix, pageNumber, pageSize)
+	range := [2]int{10, 50}
 
-	// Display the result
+	prefix_result := PREFIX_SCAN(prefix, pageNumber, pageSize)
+	range_result := RANGE_SCAN(range, pageNumber, pageSize)
+
+	// Display the result of PREFIX_SCAN
 	fmt.Printf("Results for prefix '%s', page %d, page size %d:\n", prefix, pageNumber, pageSize)
-	for _, dt := range result {
+	for _, dt := range prefix_result {
+		fmt.Printf("%s: %s\n", dt.Key, dt.Value, dt.Time)
+	}
+
+	// Display the result of RANGE_SCAN
+	fmt.Printf("Results for range ['%d', '%d'], page %d, page size %d:\n", range[0], range[1], pageNumber, pageSize)
+	for _, dt := range range_result {
 		fmt.Printf("%s: %s\n", dt.Key, dt.Value, dt.Time)
 	}
 }
