07c7db338bda20806cf4288f46739d3819501048
nikola-velemir <vnexa021@gmail.com>
2024-01-28 23:42:18 +0100

    [add] constructor for cursor

--- cursor/cursorstruct.go
+++ cursor/cursorstruct.go
@@ -7,3 +7,31 @@ type Cursor struct {
 	memIndex    int
 	lruPointer  *lru.LRUCache
 }
+
+func (c *Cursor) MemPointers() []*interface{} {
+	return c.memPointers
+}
+
+func (c *Cursor) SetMemPointers(memPointers []*interface{}) {
+	c.memPointers = memPointers
+}
+
+func (c *Cursor) MemIndex() int {
+	return c.memIndex
+}
+
+func (c *Cursor) SetMemIndex(memIndex int) {
+	c.memIndex = memIndex
+}
+
+func (c *Cursor) LruPointer() *lru.LRUCache {
+	return c.lruPointer
+}
+
+func (c *Cursor) SetLruPointer(lruPointer *lru.LRUCache) {
+	c.lruPointer = lruPointer
+}
+
+func NewCursor(memPointers []*interface{}, memIndex int, lruPointer *lru.LRUCache) *Cursor {
+	return &Cursor{memPointers: memPointers, memIndex: memIndex, lruPointer: lruPointer}
+}
