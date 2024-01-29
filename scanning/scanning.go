ca17ec9a80e646866a43da5b7799f60dab4ce816
Tetak1389 <nikolaking88@gmail.com>
2024-01-29 12:37:33 +0100

    Postavljen range scan...

--- scanning/scanning.go
+++ scanning/scanning.go
@@ -14,10 +14,6 @@ func PREFIX_SCAN(prefix string, pageNumber, pageSize int, cursor *cursor.Cursor)
 	var result []*datatype.DataType
 	var dt *datatype.DataType
 
-	// Implement pagination
-	startIndex := (pageNumber - 1) * pageSize
-	endIndex := startIndex + pageSize
-
 	n := pageNumber * pageSize
 
 	j := cursor.MemIndex()
@@ -56,7 +52,81 @@ func PREFIX_SCAN(prefix string, pageNumber, pageSize int, cursor *cursor.Cursor)
 	path := ""
 
 	for (len(result)) < n {
-		ssPodaci, offset, path = sstable.CitajPodateke(prefix, n-len(memPodaci)-len(kesPodaci), offset, path)
+		ssData, offset, path = sstable.GetDataByPrefix(prefix, n, offset, path)
+		for _, dt = range ssData {
+			if slices.Contains(result, dt) == false && dt.IsDeleted() == false {
+				result = append(result, dt)
+				n -= 1
+				if n == 0 {
+					break
+				}
+			}
+		}
+		if n == 0 {
+			break
+		}
+	}
+
+	// Implement pagination
+	startIndex := (pageNumber - 1) * pageSize
+	endIndex := startIndex + pageSize
+
+	// Sort the result by key
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].GetKey() < result[j].GetKey()
+	})
+
+	// Return the paginated result
+	return result[startIndex:endIndex]
+}
+
+func RANGE_SCAN(keyRange [2]string, pageNumber, pageSize int, cursor *cursor.Cursor) []*datatype.DataType {
+	var result []*datatype.DataType
+	var dt *datatype.DataType
+
+	// Implement pagination
+	startIndex := (pageNumber - 1) * pageSize
+	endIndex := startIndex + pageSize
+
+	n := pageNumber * pageSize
+
+	j := cursor.MemIndex()
+	for true {
+		lista := cursor.MemPointers()[j].GetElementByRange(keyRange)
+		for _, dt = range lista {
+			if dt.IsDeleted() == false && slices.Contains(result, dt) == false {
+				result = append(result, dt)
+				n -= 1
+				if n == 0 {
+					break
+				}
+			}
+		}
+		j = (j - 1 + len(cursor.MemPointers())) % len(cursor.MemPointers())
+		if j == cursor.MemIndex() {
+			break
+		}
+		if n == 0 {
+			break
+		}
+	}
+
+	lruData := cursor.LruPointer().GetAll()
+	for _, dt = range lruData {
+		if slices.Contains(result, dt) == false && strings.HasPrefix(dt.GetKey(), prefix) && dt.IsDeleted() == false {
+			result = append(result, dt)
+			n -= 1
+			if n == 0 {
+				break
+			}
+		}
+	}
+
+	offset := 0
+	path := ""
+
+	for (len(result)) < n {
+		ssPodaci, offset, path = sstable.GetDataByRange(keyRange, n, offset, path)
 		for _, dt = range ssPodaci {
 			if slices.Contains(result, dt) == false && dt.IsDeleted() == false {
 				result = append(result, dt)
@@ -77,23 +147,6 @@ func PREFIX_SCAN(prefix string, pageNumber, pageSize int, cursor *cursor.Cursor)
 	})
 
 	// Return the paginated result
-	return result[startIndex:endIndex]
-}
-
-func RANGE_SCAN(keyRange [2]string, pageNumber, pageSize int, cursor *cursor.Cursor) []*datatype.DataType {
-	var result []*datatype.DataType
-
-	startIndex := (pageNumber - 1) * pageSize
-	endIndex := startIndex + pageSize
-
-	// Sort the result by key
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].GetKey() < result[j].GetKey()
-	})
-
-	if endIndex > len(result) {
-		endIndex = len(result)
-	}
 	return result[startIndex:endIndex]
 }
 
