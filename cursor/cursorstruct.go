1d1413fd8bae6057afa2de43460db65a82673b4f
nikola-velemir <vnexa021@gmail.com>
2024-01-29 14:29:49 +0100

    [fix] cursor

--- cursor/cursorstruct.go
+++ cursor/cursorstruct.go
@@ -1,37 +1,17 @@
 package cursor
 
-import "scanning/lru"
+import (
+	"scanning/lru"
+	"scanning/mem/memtable/btree/btreemem"
+	"scanning/mem/memtable/hash/hashmem"
+	"scanning/mem/memtable/skiplist/skiplistmem"
+)
 
 type Cursor struct {
-	memPointers []*interface{}
-	memIndex    int
-	lruPointer  *lru.LRUCache
-}
-
-func (c *Cursor) MemPointers() []*interface{} {
-	return c.memPointers
-}
-
-func (c *Cursor) SetMemPointers(memPointers []*interface{}) {
-	c.memPointers = memPointers
-}
-
-func (c *Cursor) MemIndex() int {
-	return c.memIndex
-}
-
-func (c *Cursor) SetMemIndex(memIndex int) {
-	c.memIndex = memIndex
-}
-
-func (c *Cursor) LruPointer() *lru.LRUCache {
-	return c.lruPointer
-}
-
-func (c *Cursor) SetLruPointer(lruPointer *lru.LRUCache) {
-	c.lruPointer = lruPointer
-}
-
-func NewCursor(memPointers []*interface{}, memIndex int, lruPointer *lru.LRUCache) *Cursor {
-	return &Cursor{memPointers: memPointers, memIndex: memIndex, lruPointer: lruPointer}
+	memFlag             bool
+	hashMemPointers     []*hashmem.Memtable
+	breeMemPointers     []*btreemem.BTreeMemtable
+	skiplistMemPointers []*skiplistmem.SkipListMemtable
+	memIndex            int
+	lruPointer          *lru.LRUCache
 }
