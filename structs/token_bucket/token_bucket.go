85c07f7bf9f23499ee9b2a4b0ebd0a9c443143fa
Tetak1389 <nikolaking88@gmail.com>
2024-01-30 22:46:13 +0100

    Dovrsavanje Token Baketa i CMS-a.

--- structs/token_bucket/token_bucket.go
+++ structs/token_bucket/token_bucket.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
-// TokenBucket struktura
+// TokenBucket structure
 type TokenBucket struct {
 	rate                int64
 	maxTokens           int64
@@ -18,6 +18,7 @@ type TokenBucket struct {
 	lastRefillTimestamp time.Time
 }
 
+// Default Constructor
 func NewTokenBucket(rate, maxTokens int64) *TokenBucket {
 	return &TokenBucket{
 		rate:                rate,
@@ -27,6 +28,7 @@ func NewTokenBucket(rate, maxTokens int64) *TokenBucket {
 	}
 }
 
+// Function used to refill token bucket
 func (tb *TokenBucket) refill() {
 	now := time.Now()
 	end := time.Since(tb.lastRefillTimestamp)
@@ -40,109 +42,107 @@ func (tb *TokenBucket) refill() {
 
 func (tb *TokenBucket) IsRequestAllowed(tokens int64) string {
 	tb.refill()
-	data, _ := tb.Deserialize()
-	for _, value := range data {
-		fmt.Println(value)
-	}
-	data = append(data, Log{Hours: time.Now().Hour(), Minutes: time.Now().Minute(), Seconds: time.Now().Second(), Value: tokens})
-	serializedData, _ := tb.Serialize(data)
-	tb.WriteToFile("requests.bin", serializedData)
 	if tb.currentTokens >= tokens {
 		tb.currentTokens -= tokens
+		tb.AppendRequest("token_bucket/requests.bin", []byte(time.Now().Format("15:04:05")+", "+strconv.Itoa(int(tokens))+", ALLOWED\n"))
 		return "Request Allowed"
 	}
+	tb.AppendRequest("token_bucket/requests.bin", []byte(time.Now().Format("15:04:05")+", "+strconv.Itoa(int(tokens))+", BLOCKED\n"))
 	return "Request Blocked"
 }
 
-type Log struct {
-	Hours   int
-	Minutes int
-	Seconds int
-	Value   int64
-}
+// SerializeTokenBucket serializes the TokenBucket
+func (tb *TokenBucket) SerializeTokenBucket() ([]byte, error) {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
 
-func (tb *TokenBucket) Serialize(data []Log) ([]byte, error) {
-	var lines []string
+	err := encoder.Encode(struct {
+		Rate                int64
+		MaxTokens           int64
+		CurrentTokens       int64
+		LastRefillTimestamp time.Time
+	}{tb.rate, tb.maxTokens, tb.currentTokens, tb.lastRefillTimestamp})
 
-	for _, entry := range data {
-		line := fmt.Sprintf("%02d:%02d:%02d,%d", entry.Hours, entry.Minutes, entry.Seconds, entry.Value)
-		lines = append(lines, line)
-	}
-
-	return []byte(strings.Join(lines, "\n")), nil
-}
-
-func (tb *TokenBucket) WriteToFile(filename string, data []byte) error {
-	return ioutil.WriteFile(filename, data, 0644)
-}
-
-func (tb *TokenBucket) Deserialize() ([]Log, error) {
-
-	file, err := os.Open("requests.bin")
 	if err != nil {
 		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+// DeserializeTokenBucket deserializes the TokenBucket
+func DeserializeTokenBucket(data []byte) (*TokenBucket, error) {
+	var tb TokenBucket
+	buffer := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buffer)
+
+	temp := struct {
+		Rate                int64
+		MaxTokens           int64
+		CurrentTokens       int64
+		LastRefillTimestamp time.Time
+	}{}
+
+	err := decoder.Decode(&temp)
+	if err != nil {
+		return nil, err
+	}
+
+	tb.rate = temp.Rate
+	tb.maxTokens = temp.MaxTokens
+	tb.currentTokens = temp.CurrentTokens
+	tb.lastRefillTimestamp = temp.LastRefillTimestamp
+
+	return &tb, nil
+}
+
+// AppendRequest appends binary data to a requests.bin file
+func (tb *TokenBucket) AppendRequest(filename string, data []byte) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return err
 	}
 	defer file.Close()
 
-	fileInfo, err := file.Stat()
+	_, err = file.Write(data)
+	return err
+}
+
+// InitRequestsFile creates empty file for requests
+func (tb *TokenBucket) InitRequestsFile(filename string) error {
+	file, err := os.Create(filename)
 	if err != nil {
-		return nil, err
+		return err
 	}
+	defer file.Close()
 
-	fileSize := fileInfo.Size()
-	readData := make([]byte, fileSize)
+	return nil
+}
 
-	_, err = file.Read(readData)
-	if err != nil {
-		return nil, err
-	}
-
-	var result []Log
-
-	dataStr := string(readData)
-
-	// Split lines
-	lines := strings.Split(dataStr, "\n")
-
-	for _, line := range lines {
-		// Split each line into hours:minutes,value
-		parts := strings.Split(line, ",")
-
-		// Extract hours, minutes, and value from the split parts
-		timeParts := strings.Split(parts[0], ":")
-		hours, err := strconv.Atoi(timeParts[0])
-		if err != nil {
-			return nil, err
-		}
-
-		minutes, err := strconv.Atoi(timeParts[1])
-		if err != nil {
-			return nil, err
-		}
-
-		seconds, err := strconv.Atoi(timeParts[2])
-		if err != nil {
-			return nil, err
-		}
-
-		value, err := strconv.Atoi(parts[1])
-		if err != nil {
-			return nil, err
-		}
-
-		// Create a MyData instance and add it to the result
-		result = append(result, Log{Hours: hours, Minutes: minutes, Seconds: seconds, Value: int64(value)})
-	}
-
-	return result, nil
+// ToString returns a string representation of the TokenBucket
+func (tb *TokenBucket) ToString() string {
+	return fmt.Sprintf("Rate: %d, MaxTokens: %d, CurrentTokens: %d, LastRefillTimestamp: %v",
+		tb.rate, tb.maxTokens, tb.currentTokens, tb.lastRefillTimestamp.Format("15:04:05"))
 }
 
 func main() {
+
 	tb := NewTokenBucket(3, 10)
+	err := tb.InitRequestsFile("token_bucket/requests.bin")
 
-	for i := 1; i <= 30; i++ {
-		fmt.Println(i, tb.IsRequestAllowed(4), " at ", time.Now().Format("15:04:05"))
-		time.Sleep(1000 * time.Millisecond)
+	if err != nil {
+		fmt.Println("Unsuccessful initialization of the requests.bin file!")
+		return
 	}
-}
 
+	for i := 1; i <= 15; i++ {
+		fmt.Println(tb.IsRequestAllowed(2), " at ", time.Now().Format("15:04:05"))
+		time.Sleep(300 * time.Millisecond)
+	}
+
+	data, _ := tb.SerializeTokenBucket()
+
+	tb2, _ := DeserializeTokenBucket(data)
+
+	fmt.Println((*tb2).ToString())
+}
